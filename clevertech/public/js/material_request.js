823ea75ac02da6a9ee11f7b08d04d68ac3e7cee3
Yogesh <yogesh.transcendent.co.in>
2025-11-29 16:32:27 +0530

    Update material request JS and add validation for Purchase Type

--- clevertech/public/js/material_request.js
+++ clevertech/public/js/material_request.js
@@ -2,15 +2,114 @@ frappe.ui.form.on('Material Request', {
         refresh(frm) {
                 frm.events.make_custom_buttons(frm);
         },
-        make_custom_buttons: function (frm) {
+	make_custom_buttons: function (frm) {
         if (frm.doc.docstatus == 0) {
+            frm.add_custom_button(
+                    __("Bill of Materials"),
+                    () => frm.events.get_items_from_bom(frm),
+                    __("Get Items From")
+            );
+        }
+
+        if (frm.doc.docstatus == 1 && frm.doc.status != "Stopped") {
+            let precision = frappe.defaults.get_default("float_precision");
+
+            if (flt(frm.doc.per_received, precision) < 100) {
+                frm.add_custom_button(__("Stop"), () => frm.events.update_status(frm, "Stopped"));
+            }
+
+            if (flt(frm.doc.per_ordered, precision) < 100) {
+                let add_create_pick_list_button = () => {
+                    frm.add_custom_button(
+                            __("Pick List"),
+                            () => frm.events.create_pick_list(frm),
+                            __("Create")
+                    );
+                };
+
+                if (frm.doc.material_request_type === "Material Transfer") {
+                    add_create_pick_list_button();
+                    frm.add_custom_button(
+                        __("Material Transfer"),
+                        () => frm.events.make_stock_entry(frm),
+                        __("Create")
+                    );
+
+                    frm.add_custom_button(
+                        __("Material Transfer (In Transit)"),
+                        () => frm.events.make_in_transit_stock_entry(frm),
+                        __("Create")
+                    );
+                }
+
+                if (frm.doc.material_request_type === "Material Issue") {
+                    frm.add_custom_button(
+                        __("Issue Material"),
+                        () => frm.events.make_stock_entry(frm),
+                        __("Create")
+                    );
+                }
+
+                if (frm.doc.material_request_type === "Customer Provided") {
+                    frm.add_custom_button(
+                            __("Material Receipt"),
+                            () => frm.events.make_stock_entry(frm),
+                            __("Create")
+                    );
+                }
+
+                if (frm.doc.material_request_type === "Purchase") {
+                    frm.add_custom_button(
+                            __("Purchase Order"),
+                            () => frm.events.make_purchase_order(frm),
+                            __("Create")
+                    );
+
+                    frm.add_custom_button(
+                            __("Request for Quotation"),
+                            () => frm.events.make_request_for_quotation(frm),
+                            __("Create")
+                    );
+
+                    frm.add_custom_button(
+                            __("Supplier Quotation"),
+                            () => frm.events.make_supplier_quotation(frm),
+                            __("Create")
+                    );
+                }
+
+                if (frm.doc.material_request_type === "Manufacture") {
+                    frm.add_custom_button(
+                            __("Work Order"),
+                            () => frm.events.raise_work_orders(frm),
+                            __("Create")
+                    );
+                }
+
+                if (frm.doc.material_request_type === "Subcontracting") {
+                    frm.add_custom_button(
+                            __("Subcontracted Purchase Order"),
+                            () => frm.events.make_purchase_order(frm),
+                            __("Create")
+                    );
+                }
+
+                frm.page.set_inner_btn_group_as_primary(__("Create"));
+                }
+        }
+
+        if (frm.doc.docstatus === 0) {
                 frm.add_custom_button(
-                        __("Bill of Materials"),
-                        () => frm.events.get_items_from_bom(frm),
+                        __("Sales Order"),
+                        () => frm.events.get_items_from_sales_order(frm),
                         __("Get Items From")
                 );
         }
-        },
+
+        if (frm.doc.docstatus == 1 && frm.doc.status == "Stopped") {
+                frm.add_custom_button(__("Re-open"), () => frm.events.update_status(frm, "Submitted"));
+        }
+    },
         get_items_from_bom: function (frm) {
         var d = new frappe.ui.Dialog({
             title: __("Get Items from BOM"),
@@ -59,7 +158,7 @@ frappe.ui.form.on('Material Request', {
                                 d.item_name = item.item_name;
                                 d.description = item.description;
                                 d.warehouse = values.warehouse;
-                                d.uom = item.stock_uom;
+                               d.uom = item.stock_uom;
                                 d.stock_uom = item.stock_uom;
                                 d.conversion_factor = 1;
                                 d.qty = item.qty;
@@ -78,7 +177,7 @@ frappe.ui.form.on('Material Request', {
         d.show();
     },
     validate: function(frm) {
-        // Skip if already confirmed once
+       // Skip if already confirmed once
         if (frm.skip_bom_qty_check) return;
 
         let items_exceeding = frm.doc.items.filter(i => i.bom_no && i.custom_bom_qty && i.qty > i.custom_bom_qty);
@@ -119,6 +218,9 @@ frappe.ui.form.on("Material Request Item", {
 
     qty: function(frm, cdt, cdn) {
 	    update_actual_qty(frm,cdt,cdn)
+    },
+       refresh: function(frm, cdt, cdn) {
+            update_actual_qty(frm,cdt,cdn)
     },
 });
 function update_actual_qty(frm,cdt,cdn){
--- clevertech/server_scripts/material_request.py
+++ clevertech/server_scripts/material_request.py
@@ -56,6 +56,27 @@ def before_validate(doc, method):
         sd = getdate(doc.schedule_date)
         diff = (sd - td).days
         doc.custom_required_by_in_days = diff
+    if doc.material_request_type == "Purchase":
+        settings = frappe.get_cached_doc("Quality Warehouse Settings")
+        qc_accepted = settings.qc_accepted_warehouse
+        default_store = settings.default_store_warehouse
+
+        for row in doc.items:
+            if not row.item_code:
+                continue
+
+            # Fetch only the required field
+            requires_inspection = frappe.db.get_value(
+                "Item",
+                row.item_code,
+                "inspection_required_before_purchase"
+            )
+
+            # Apply logic
+            if requires_inspection:
+                row.warehouse = qc_accepted
+            else:
+                row.warehouse = default_store
 
 
 def validate(doc,method):
